bf4b067f19b4ab8f7cdf8d6b47c94e14c6a9fca6
nealtheseal108 <nealtheseal108@gmail.com>
2022-07-22 14:55:35 -0700

    ew

---
+++ Section19/toDoList.js
@@ -0,0 +1,9 @@
+while (true) {
+    let input = prompt("What would you like to do?");
+    if (input.toLowerCase().trim() !== "new" || input.toLowerCase().trim() !== "list" || input.toLowerCase().trim() !== "remove" || input.toLowerCase().trim() !== "quit") {
+        console.log("That's not an action I can perform at the current moment.");
+    } else if ((input.toLowerCase().trim() === "new")) {
+        input = prompt("What would you like to accomplish today?");
+        let 
+    }
+}
