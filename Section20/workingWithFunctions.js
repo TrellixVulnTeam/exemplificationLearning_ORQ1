1e0d25ccf738516e930c9a49d208f3a963c32e15
nealtheseal108 <nealtheseal108@gmail.com>
2022-07-23 09:20:15 -0700

    asfds

---
+++ Section20/workingWithFunctions.js
@@ -0,0 +1,9 @@
+function singSong() {
+    console.log("do");
+    console.log("re");
+    console.log("mi");
+}
+
+function greet(firstName, lastName) {
+    console.log("firstName is " + firstName + " and lastName is " + lastName);
+}
