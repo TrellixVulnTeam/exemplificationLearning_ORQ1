c2c678b9e3870d3d45aacfa510ea4010d4d202d4
nealtheseal108 <nealtheseal108@gmail.com>
2022-08-10 10:54:01 -0700

    ewq

--- Section29/colorFunctions.js
+++ Section29/colorFunctions.js
@@ -28,3 +28,24 @@ Color.prototype.rgb = function() {
 }
 
 const var1 = new Color(0, 0, 0);
+
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+
+    greet() {
+        console.log("hi");
+    }
+
+    rgb() {
+        return "rbg(" + this.r + ", " + this.g + ", " + this.b + ")";
+    }
+
+    hex() {
+        return "#" + ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b).toString(16).slice(1);
+    }
+}
