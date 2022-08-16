ce28c3ffae0e04c8f9bf3d991f28c7b9697153d1
nealtheseal108 <nealtheseal108@gmail.com>
2022-08-16 11:31:50 -0700

    gdf

---
+++ Section32/langGuesser/index.js
@@ -0,0 +1,6 @@
+const franc = require("franc");
+const langs = require("langs");
+const input = process.argv[2];
+const langCode = franc(input);
+const language = langs.where("3", langCode);
+console.log(language.name);
